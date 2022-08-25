7b1aa42c34348a8ce58d5b0d78bd121ae95384b4
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-08-25 19:41:07 -0300

    Se agregó el modal para mostrar las recetas
    From: https://mui.com/material-ui/react-modal/

--- src/components/Recetas.js
+++ src/components/Recetas.js
@@ -1,7 +1,57 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
 
+//Modal materia ui viejo
+// import Modal from "@mui/material/Modal";
+// import { makeStyles } from "@mui/material/styles";
+
+// function getModalStyle() {
+//   const top = 50;
+//   const left = 50;
+
+//   return {
+//     top: `${top}%`,
+//     left: `${left}%`,
+//     transform: `translate(-${top}%, -${left}%)`,
+//   };
+// }
+
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     position: "absolute",
+//     width: 450,
+//     backgroundColor: theme.palette.background.paper,
+//     boxShadow: theme.shadows[5],
+//     padding: theme.spacing(2, 4, 3),
+//   },
+// }));
+
+// //
+
+//MODAL NUEVO
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 const Receta = ({ receta }) => {
+  //Configuración del modal
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   //Extraer los valores del context
   const { guardarIdReceta } = useContext(ModalContext);
 
@@ -18,10 +68,32 @@ const Receta = ({ receta }) => {
           <button
             type="button"
             className="btn btn-block btn-primary"
-            onClick={() => {guardarIdReceta(receta.idDrink)}}
+            onClick={() => {
+              guardarIdReceta(receta.idDrink);
+              handleOpen();
+            }}
           >
             Ver receta
           </button>
+
+          <Modal
+            open={open}
+            onClose={() => {
+              guardarIdReceta(null);
+              handleClose();
+            }}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
         </div>
       </div>
     </div>
